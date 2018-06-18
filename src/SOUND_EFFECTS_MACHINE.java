ab91a6fdb3e424c248a9ea62cb6b6607704b4418
SunStar56 <league@imac.attlocal.net>
2018-06-17 17:24:14 -0700

    cuteness tv, houses

--- src/SOUND_EFFECTS_MACHINE.java
+++ src/SOUND_EFFECTS_MACHINE.java
@@ -1,9 +1,11 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.applet.AudioClip;
 
 public class SOUND_EFFECTS_MACHINE implements ActionListener {
 	public static void main(String[] args) {
@@ -11,10 +13,11 @@ public class SOUND_EFFECTS_MACHINE implements ActionListener {
 		SEM.m();
 	}
 
+	JButton b = new JButton("SOUND1");
+	JButton bb = new JButton("SOUND2");
+
 	public void m() {
 		JFrame f = new JFrame();
-		JButton b = new JButton("SOUND1");
-		JButton bb = new JButton("SOUND2");
 		JPanel p = new JPanel();
 		f.setVisible(true);
 		b.addActionListener(this);
@@ -22,20 +25,23 @@ public class SOUND_EFFECTS_MACHINE implements ActionListener {
 		f.add(p);
 		p.add(b);
 		p.add(bb);
-
 		f.pack();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-	JButton buttonPressed = (JButton) e.getSource();
-if(buttonPressed.equals(b))
-{
-playSound("drum.wav");	
-}
-if(buttonPressed.equals(bb))
-{
-playSound("homer-woohoo.wav");	
-}
+		JButton buttonPressed = (JButton) e.getSource();
+		if (buttonPressed.equals(b)) {
+			playSound("drum.wav");
+		}
+		if (buttonPressed.equals(bb)) {
+			playSound("homer-woohoo.wav");
+		}
 	}
+
+	private void playSound(String fileName) {
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		sound.play();
+	}
+
 }
